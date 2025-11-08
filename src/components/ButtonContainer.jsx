7e7fe1183fe85f1b6abae6cacdd7613dd3c87a73
shakirwaheed037 <shakirwaheed1818@gmail.com>
2025-11-08 21:29:50 +0500

    updated button component

--- src/components/ButtonContainer.jsx
+++ src/components/ButtonContainer.jsx
@@ -3,10 +3,10 @@ import styles from "./ButtonContainer.module.css";
 const ButtonContainer = ({ onButtonClick }) => {
   const buttonNames = [
     "C", "1", "2", "+",
-    "3", "4", "-", 
-    "5", "6", "*", 
-    "7", "8", "/", 
-    "9", "0", ".", "="
+    "3", "4", "5", 
+    "-", "6", "7", 
+    "8", "*", "9", 
+    "0", ".", "/", "="
   ];
 
   return (
